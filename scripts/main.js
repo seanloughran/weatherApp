7320a97136d5a3edec286823013ff3ec65f78d65
seanloughran <seancloughran@gmail.com>
2016-09-26 18:11:55 -0700

    comments, loading dots

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,22 @@
 
 $(document).ready(function() {
 
+  var dots = 0;
+
+  setInterval(function() {
+    if (dots < 3) {
+      $("#dots").append(".");
+      dots++;
+    } else {
+      $("#dots").html("");
+      dots = 0;
+    }
+  }, 1000);
+
   var far = "";
   var cel = "";
-  console.log(far);
 
+  // Functionality for fahrenheit/celsius button
   $("#switch").on("click", function() {
     var type = $("#temp").attr("data-temp");
     var tem = parseInt($("#temp").html());
@@ -28,27 +40,27 @@ $(document).ready(function() {
     }
   });
 
-  if (navigator.geolocation) {
+  if (navigator.geolocation) { //checks if browser supports geolocation
     navigator.geolocation.getCurrentPosition(showLocation);
 
-    function showLocation(position) {
+    function showLocation(position) { // position fed from "getCurrentPosition"
       console.log(position);
       var lat = position.coords.latitude;
       var long = position.coords.longitude;
-      var latlng = {lat: lat, lng: long};
-      var forecastKEY = "679039406e42fb183bd1bdf8f3155b71";
+      var latlng = {lat: lat, lng: long}; // Featured in reverse geocode request
+      var forecastKEY = "679039406e42fb183bd1bdf8f3155b71"; // Key for forecast API.
       var url = "https://api.forecast.io/forecast/";
 
       // Start of reverse geo request for address.
       $.ajax({
         url: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCzgzYNnZAyw6TSIZJ46HHtjzr8puUwOpQ",
         type: "GET",
-        dataType: 'jsonp',
+        dataType: 'jsonp', // jsonp used for crossorigin request
         success: function(data) {
           var geocoder = new google.maps.Geocoder;
           geocoder.geocode({location: latlng}, function(results, status) {
             if (status === 'OK') {
-              var place = results[3].formatted_address;
+              var place = results[3].formatted_address; // Ex. Portland, OR, USA
               $("#place").html(place);
             }
           });
@@ -59,21 +71,22 @@ $(document).ready(function() {
       // End of reverse geo request for address.
 
 
-      // Start of request for weather forecast.
+      // Start of request for weather forecast call.
       $.ajax({
-        url: url + forecastKEY + "/" + lat + "," + long,
+        url: url + forecastKEY + "/" + lat + "," + long, // Calls for forecast of specific location.
         method: 'GET',
         dataType: 'jsonp',
         success: function(response) {
           // console.log(response);
           var temp = response.currently.apparentTemperature.toFixed(1);
-          far = response.currently.apparentTemperature.toFixed(1);
+          far = response.currently.apparentTemperature.toFixed(1); // Involved in fahrenheit/celsius switch.
           // console.log(far);
 
-          $("#temp").html(temp);
+          $("#temp").html(temp); // Puts temp onto page.
 
-          var icon = response.currently.icon;
+          var icon = response.currently.icon; // Icon for description of the current weather.
 
+          // Assigns background image in accordance with "icon"
           switch (icon) {
             case "clear-night":
               $("#outside-main").css("background-image", 'url("images/nightsky.jpg")');
@@ -103,7 +116,7 @@ $(document).ready(function() {
               $("#outside-main").css("background-image", 'url("images/clearday.jpg")');
           }
 
-          $("#blackBox").css("display", "none");
+          $("#blackBox").css("display", "none"); // Removes "loading" black after all information has been loaded.
         }
 
       });
@@ -112,10 +125,12 @@ $(document).ready(function() {
 
 
     }
-    // End of if navigator.geolocation
+    // End of showLocation
 
   } else {
-    $("#testP").html("Looks like your browser doesn't support geolocation");
+    // Runs if geolocation not supported
+    $("#place").html("Looks like your browser doesn't support geolocation");
   }
+  // End of if (navigator.geolocation)
 
 });
